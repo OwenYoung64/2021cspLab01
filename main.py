7adbcc4b30d4c070eaa7a94916a3318033fd6b17
Owen Young <owen.young@vtsd.com>
2021-10-19 13:30:48 -0400

    commit

--- main.py
+++ main.py
@@ -1,5 +1,28 @@
+import turtle as trtl
+
 def main():
-    print("Lab's work goes in the main function")
+    #create turtle and set up box
+    gary = trtl.Turtle()
+    box(gary)
+
+#draw the lines
+
+    wn = trtl.Screen()
+    wn.mainloop()
+
+def box(gary):
+    xpos = -500
+    ypos = -300
+    gary.speed(0)
+    gary.penup()
+    gary.goto(xpos,ypos)
+    gary.pendown()
+    gary.goto(xpos+980, ypos)
+    gary.goto(xpos+980, ypos+630)
+    gary.goto(xpos, ypos+630)
+    gary.goto(xpos, ypos)
+
+
 
 if __name__ == "__main__":
     main()
