b818622e62a988f358fe64019d2083f0133ac8ac
Owen Young <90921938+OwenYoung64@users.noreply.github.com>
2021-10-25 20:18:30 +0000

    110% completed!

--- main.py
+++ main.py
@@ -7,8 +7,12 @@ def main():
     xpos = -500
     ypos = -300
 
-    #draw the lines
+    #draw the lies
+    #making the first box
 
+    xpos = -500
+    ypos = -300
+    gary.goto(xpos, ypos)
     while (gary.ycor() < 330):
         gary.penup()
         gary.goto(xpos, -300)
@@ -53,6 +57,57 @@ def main():
         gary.penup()
         xpos -= 15.4
         ypos -= 10
+    
+    #making the second box
+    box2(gary)
+
+    xpos = -250
+    ypos = -145
+    gary.goto(xpos, ypos)
+    while (gary.ycor() < 170):
+        gary.penup()
+        gary.goto(xpos, -145)
+        gary.pendown()
+        gary.goto(230, ypos+5)
+        gary.penup()
+        xpos += 7.7
+        ypos += 5
+    
+    xpos = 230
+    ypos = -145
+    gary.goto(230, ypos)
+    while (gary.ycor() < 170):
+        gary.penup()
+        gary.goto(xpos, -145)
+        gary.pendown()
+        gary.goto(-250, ypos+5)
+        gary.penup()
+        xpos -= 7.7
+        ypos += 5
+
+    xpos = -250
+    ypos = 170
+    gary.goto(-250, ypos)
+    while (gary.ycor() > -145):
+        gary.penup()
+        gary.goto(xpos, 170)
+        gary.pendown()
+        gary.goto(230, ypos - 5)
+        gary.penup()
+        xpos += 7.7
+        ypos -= 5
+
+    xpos = 230
+    ypos = 170
+    gary.goto(230, ypos)
+    while (gary.ycor() > -145):
+        gary.penup()
+        gary.goto(xpos, 170)
+        gary.pendown()
+        gary.goto(-250, ypos - 5)
+        gary.penup()
+        xpos -= 7.7
+        ypos -= 5
 
 
 
@@ -70,6 +125,18 @@ def box(gary):
     gary.goto(xpos+980, ypos)
     gary.goto(xpos+980, ypos+630)
     gary.goto(xpos, ypos+630)
+    gary.goto(xpos, ypos)
+
+def box2(gary):
+    xpos = -250
+    ypos = -145
+    gary.speed(0)
+    gary.penup()
+    gary.goto(xpos,ypos)
+    gary.pendown()
+    gary.goto(xpos+480, ypos)
+    gary.goto(xpos+480, ypos+315)
+    gary.goto(xpos, ypos+315)
     gary.goto(xpos, ypos)
 
 
